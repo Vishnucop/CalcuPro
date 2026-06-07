d528b02c1f3d5b3f0aeea9d3ac93c81c36574dd0
Vishnucop <martinezluca9422@gmail.com>
2026-06-07 05:10:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -59,3 +59,64 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+const CACHE_NAME = 'calcupro-v2';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './css/styles.css',
+    './js/app.js',
+    './js/calculators/estandar.js',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+// Instalar SW
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache abierto');
+                return cache.addAll(urlsToCache.map(url => {
+                    return new Request(url, { cache: 'reload' });
+                }));
+            })
+            .catch(err => {
+                console.log('Error al cachear:', err);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Activar SW
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Interceptar requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).catch(() => {
+                    if (event.request.mode === 'navigate') {
+                        return caches.match('./index.html');
+                    }
+                });
+            })
+    );
+});
